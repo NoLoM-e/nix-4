5aa895fb7556d46e73c7ec1ff7fb6ff11f8af762
NoLoM <dyjey18@gmail.com>
2021-02-28 22:51:58 +0300

    added libs realisation

--- console/src/com/company/Class2.java
+++ console/src/com/company/Class2.java
@@ -7,5 +7,7 @@ public class Class2
     public void test()
     {
         System.out.println("Class2 also works propperly");
+
+        System.out.println(StringUtils.abbreviate("Hello World!", 8));
     }
 }
--- console/src/com/company/Main.java
+++ console/src/com/company/Main.java
@@ -11,6 +11,10 @@ public class Main
         Class1 class1 = new Class1();
         Class2 class2 = new Class2();
 
+        Complex z1 = new Complex(1.5, 21.7);
+        Complex z2 = new Complex(3.21, 1.11);
+        Complex z3 = z1.add(z2);
 
+        Sytem.out.println("(1.5 + 21i) + (3.21 + 1.11i) = " + z3.getReal() + " " + z3.getImaginary());
     }
 }
