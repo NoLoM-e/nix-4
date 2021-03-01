569238c3dfc9c85e2e06848a61e19a34dd02d9ad
NoLoM <dyjey18@gmail.com>
2021-03-01 20:11:36 +0300

    copied project to maven structure, edited pom, added maven to run.bat

---
+++ maven/app/src/main/java/com/company/App.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.company.Class2;
+import com.company.class1.Class1;
+import org.apache.commons.math3.complex.*;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        Class1 class1 = new Class1();
+        Class2 class2 = new Class2();
+
+        class1.test();
+        class2.test();
+
+        Complex z1 = new Complex(1.5, 21.7);
+        Complex z2 = new Complex(3.21, 1.11);
+        Complex z3 = z1.add(z2);
+
+        System.out.println("(1.5 + 21i) + (3.21 + 1.11i) = " + z3.getReal() + " " + z3.getImaginary());
+    }
+}
--- run.bat
+++ run.bat
@@ -1,12 +1,11 @@
 mkdir console\build
 mkdir console\build\classes
-mkdir console\build\jar
 javac -sourcepath console\src -d console\build\classes -cp console\libs\commons-lang3-3.11.jar;console\libs\commons-math3-3.6.1.jar console\src\com\company\Class2.java console\src\com\company\Main.java console\src\com\company\class1\Class1.java
 java -cp console\build\classes\;console\libs\commons-lang3-3.11.jar;console\libs\commons-math3-3.6.1.jar com.company.Main
+echo -------------------------------------------------------------------------------------------------
+cd maven\app
+call mvn clean install
+java -jar target\app-1.0-SNAPSHOT.jar
+cd ..\..\
+echo -------------------------------------------------------------------------------------------------
 pause
-
-
-
-commons-lang3-3.11.jar
-
-commons-math3-3.6.1.jar
