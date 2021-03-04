b8437be9b6a684c7026d0aea6a356ce24deec8e4
NoLoM <dyjey18@gmail.com>
2021-03-04 09:01:20 +0300

    added project and gitignore file

---
+++ ant/src/com/company/Class2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.apache.commons.lang3.*;
+
+public class Class2
+{
+    public void test()
+    {
+        System.out.println("Class2 also works propperly");
+
+        System.out.println(StringUtils.abbreviate("Hello World!", 8));
+    }
+}
