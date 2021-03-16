3d9f84807f5da75a7929ca45aeeb85fddd412e41
NoLoM <dyjey18@gmail.com>
2021-03-16 12:21:48 +0300

    added coutChars method

--- unit_2_algorithmic_tasks/src/com/company/Main.java
+++ unit_2_algorithmic_tasks/src/com/company/Main.java
@@ -15,6 +15,9 @@ class Main
 
         System.out.println("Enter str for task 1:");
         countString(s.next());
+
+        System.out.println("Enter str for task 2:");
+        countChars(s.next());
     }
 
     public static void countString(String str)
@@ -29,5 +32,28 @@ class Main
         }
 
         System.out.println("sum = " + sum);
+    }
+
+    public static void countChars(String str)
+    {
+        str = str.replaceAll("[^a-zA-Zа-яА-Я]", "");
+
+        while (!str.equals(""))
+        {
+            char c = str.charAt(0);
+            int counter = 0;
+
+            for(int i = 0; i < str.length(); i++)
+            {
+                if(str.charAt(i) == c)
+                {
+                    counter++;
+                }
+            }
+
+            System.out.println(c + " - " + counter);
+
+            str = str.replaceAll(String.valueOf(c), "");
+        }
     }
 }
