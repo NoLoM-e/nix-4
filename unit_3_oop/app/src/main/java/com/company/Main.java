a81ffa920f8345c648421de8f94d161fe0618f6a
NoLoM <dyjey18@gmail.com>
2021-03-19 23:01:15 +0300

    finished project

--- unit_3_oop/app/src/main/java/com/company/Main.java
+++ unit_3_oop/app/src/main/java/com/company/Main.java
@@ -1,4 +1,10 @@
 package com.company;
 
+import com.company.control.CalcController;
+
 public class Main {
+    public static void main(String[] args) {
+        CalcController calcController = new CalcController();
+        calcController.run();
+    }
 }
